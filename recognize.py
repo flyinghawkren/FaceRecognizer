239af67c6c922f88437922d6f958db588a07915a
10033385 <gong.mingsheng@zte.com.cn>
2018-12-09 17:48:30 +0800

    refactor recognize

--- recognize.py
+++ recognize.py
@@ -7,24 +7,28 @@ from FaceDetector import BaiduFaceDetector
 from FaceRecognizer import BaiduFaceRecognizer
 from Capture import VideoCapture
 
-subject = Subject()
-capture = VideoCapture(subject)
+def main():
+    subject = Subject()
+    capture = VideoCapture(subject)
 
-def show_result(result):
-    print(result)
-    capture.setResult(result)
+    def show_result(result):
+        print(result)
+        capture.setResult(result)
 
-def on_error(err):
-    print('Error ocured:', err)
+    def on_error(err):
+        print('Error ocured:', err)
 
 
-detector = BaiduFaceDetector()
-recognizer = BaiduFaceRecognizer()
+    detector = BaiduFaceDetector()
+    recognizer = BaiduFaceRecognizer()
 
-newframe = subject.throttle_last(2000) \
-    .map(lambda frame: detector.run(frame)) \
-    .filter(lambda x: detector.successDetectFace(x)) \
-    .map(lambda x: recognizer.run(x)) \
-    .subscribe(show_result, on_error)
+    newframe = subject.throttle_last(2000) \
+        .map(lambda frame: detector.run(frame)) \
+        .filter(lambda x: detector.successDetectFace(x)) \
+        .map(lambda x: recognizer.run(x)) \
+        .subscribe(show_result, on_error)
 
-capture.run()
+    capture.run()
+
+if __name__ == '__main__':
+    main()
